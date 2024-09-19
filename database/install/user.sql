a957970e30e8004251d9fbc85fed61d5f7367c59
Felipe Cavalca <flpsnocvla@gmail.com>
2024-09-19 19:56:04 -0300

    Criando tabela de usuarios

--- database/install/user.sql
+++ database/install/user.sql
@@ -1,14 +1,12 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS user (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     username VARCHAR(50) UNIQUE NOT NULL CHECK (username ~ '^[a-z0-9_]{3,50}$'),
     email VARCHAR(100) UNIQUE NOT NULL,
-    password TEXT NOT NULL,
-    home_directory VARCHAR(255) NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    password TEXT NOT NULL
 );
 
--- Exemplo para criar um trigger para a tabela 'users'
-SELECT create_log_trigger('users');
+SELECT create_log_trigger('user');
 
-INSERT INTO users (name, username, email, password, home_directory) VALUES ('admin', 'admin', 'admin@dossier.com', '123456', '/var/lib/dav/data/admin');
+-- senha padrão: 123456
+INSERT INTO user (name, username, email, password) VALUES ('admin', 'admin', 'admin@dossier.com', '$2y$10$Vc3bOeteb7tGZ9FiYt5ZAOMmIQ69Xp5hxpwQ7davtvWurEaypzXH2');
