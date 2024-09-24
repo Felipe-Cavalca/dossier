42f717010cc4f4405a4ec0a90d84c5042d8e835d
Felipe Cavalca <flpsnocvla@gmail.com>
2024-09-23 22:37:59 -0300

    Criando endpoint para criar pastas

--- api/Controller/folder.php
+++ api/Controller/folder.php
@@ -2,13 +2,15 @@
 
 namespace Bifrost\Controller;
 
+use Bifrost\Core\Database;
 use Bifrost\Attributes\Cache;
-use Bifrost\Interface\ControllerInterface;
 use Bifrost\Attributes\Method;
+use Bifrost\Attributes\RequiredFields;
 use Bifrost\Attributes\RequiredParams;
 use Bifrost\Class\HttpResponse;
 use Bifrost\Class\HttpError;
 use Bifrost\Class\Folder as FolderClass;
+use Bifrost\Interface\ControllerInterface;
 
 class Folder implements ControllerInterface
 {
@@ -34,6 +36,37 @@ class Folder implements ControllerInterface
 
         return HttpResponse::success(
             message: "Listagem de pastas",
+            data: $folder
+        );
+    }
+
+    #[Method("POST")]
+    #[RequiredFields([
+        "user_id" => FILTER_VALIDATE_INT,
+        // "parent_id" => FILTER_VALIDATE_INT,
+        "name" => FILTER_SANITIZE_SPECIAL_CHARS,
+    ])]
+    public function create(): array
+    {
+        $database = new Database();
+        $folder = new FolderClass();
+
+        $database->setSystemIdentifier([
+            "user_id" => $_POST["user_id"]
+        ]);
+
+        $dataInsert = [
+            "user_id" => (int) $_POST["user_id"],
+            "parent_id" => (int) $_POST["parent_id"] ?? null,
+            "name" => (string) $_POST["name"]
+        ];
+
+        $insert = $database->insert("folder", $dataInsert);
+
+        $folder = new FolderClass($insert, $_POST["user_id"]);
+
+        return HttpResponse::success(
+            message: "Pasta criada com sucesso",
             data: $folder
         );
     }
