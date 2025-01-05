5241be3691b2a6bbe239d2146732788e25b0a152
Felipe Cavalca <flpsnoccvla@gmail.com>
2025-01-05 01:37:47 -0300

    Adicionando validação e feedback ao cadastrar um usuario

--- app/js/pages/cadastro.js
+++ app/js/pages/cadastro.js
@@ -3,14 +3,43 @@ const bifrost = new Bifrost(
         bifrost.replaceTextInElement("body", bifrost.config);
     },
     (bifrost) => {
+        let before = () => {
+            let invalido = document.querySelector("input:invalid");
+            if (invalido) {
+                invalido.focus();
+                return false;
+            }
+            return true;
+        };
+        let after = async (response) => {
+            let elementHeader = document.querySelector("div[slot='header']");
+            let elementResponse = document.querySelector("pre[slot='body']");
+            let elementAlert = document.querySelector("c-alert");
+            if (response.ok) {
+                elementResponse.innerHTML = "Redirecionando para a página de login...";
+                elementHeader.innerHTML = "Usuário cadastrado com sucesso";
+                elementAlert.style.display = "block";
+                setTimeout(() => {
+                    window.location.href = "/";
+                }, 3000);
+            } else {
+                elementResponse.innerHTML = JSON.stringify(await response.json(), null, 2);
+                elementHeader.innerHTML = "Houve um erro ao cadastrar o usuário";
+                elementAlert.style.display = "block";
+            }
+        };
 
+        bifrost.form("form", before, after);
     }
 );
 
 function toInput(element) {
     window.location.href = element;
     setTimeout(() => {
-        document.querySelector(`${element} input`).focus();
+        let input = document.querySelector(`${element} input`);
+        if(input) {
+            input.focus();
+        }
     }, 500);
 }
 
