e15e6eea1dba17c32a1cc84936e2f525745db874
Felipe Cavalca <flpsnocvla@gmail.com>
2025-01-22 22:54:42 -0300

    Criar redirecionamento no js caso o login seja feito com sucesso

--- app/js/pages/index.js
+++ app/js/pages/index.js
@@ -7,9 +7,13 @@ const bifrost = new Bifrost(
         bifrost.form("form", () => {
             // return false;
         }, async (response) => {
+            let jsonRes = await response.json();
             document.querySelector("c-alert").style.display = 'block'
+            document.querySelector("#response-form").innerHTML = jsonRes.message;
 
-            document.querySelector("#response-form").innerHTML = await response.text();
+            if (jsonRes.isSuccess) {
+                window.location.href = './home.html';
+            }
         });
     }
 );
