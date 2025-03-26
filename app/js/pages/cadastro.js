e54a59400704acf9a8151497916b3ffd13fd0b2e
Felipe Cavalca <flpsnocvla@gmail.com>
2025-03-26 02:22:59 -0300

    Refatora lógica de envio do formulário e adiciona feedback visual para o usuário

--- app/js/pages/cadastro.js
+++ app/js/pages/cadastro.js
@@ -1,55 +1,42 @@
-const bifrost = new Bifrost(
-    (bifrost) => {
-        bifrost.replaceTextInElement("body", bifrost.config);
-    },
-    (bifrost) => {
-        let before = () => {
-            let invalido = document.querySelector("input:invalid");
-            if (invalido) {
-                invalido.focus();
-                return false;
-            }
-            return true;
-        };
-        let after = async (response) => {
-            let elementHeader = document.querySelector("div[slot='header']");
-            let elementResponse = document.querySelector("pre[slot='body']");
-            let elementAlert = document.querySelector("c-alert");
-            if (response.ok) {
-                elementResponse.innerHTML = "Redirecionando para a página de login...";
-                elementHeader.innerHTML = "Usuário cadastrado com sucesso";
-                elementAlert.style.display = "block";
-                setTimeout(() => {
-                    window.location.href = "/";
-                }, 3000);
-            } else {
-                elementResponse.innerHTML = JSON.stringify(await response.json(), null, 2);
-                elementHeader.innerHTML = "Houve um erro ao cadastrar o usuário";
-                elementAlert.style.display = "block";
-            }
-        };
+var originalTextButton;
 
-        bifrost.form("form", before, after);
-    }
-);
-
-function toInput(element) {
-    window.location.href = element;
-    setTimeout(() => {
-        let input = document.querySelector(`${element} input`);
-        if(input) {
-            input.focus();
-        }
-    }, 500);
+const beforeLoad = (bifrost) => {
+    bifrost.replaceTextInElement("body", bifrost.config);
 }
 
-function focusOnEnter(event, nextElement) {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        toInput(nextElement);
+const afterLoad = (bifrost) => {
+    bifrost.form("form", beforeSend, afterSend);
+}
+
+const beforeSend = () => {
+    let invalido = document.querySelector("input:invalid");
+
+    if (invalido) {
+        alert().error("Preencha todos os campos corretamente");
+        return false;
+    }
+
+    let button = document.querySelector("button[type=submit]");
+    button.disabled = true;
+    originalTextButton = button.innerHTML;
+    button.innerHTML = "Enviando...";
+
+    return true;
+}
+
+const afterSend = async (response) => {
+    let data = await response.json();
+
+    let button = document.querySelector("button[type=submit]");
+    button.disabled = false;
+    button.innerHTML = originalTextButton;
+
+    if (data.isSuccess) {
+        alert().success(data.message);
+        document.querySelector("form").reset();
+    } else {
+        alert().error(data.message);
     }
 }
 
-function focusOnButton(nextElement) {
-    toInput(nextElement);
-}
+new Bifrost(beforeLoad, afterLoad);
