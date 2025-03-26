0e6b8f564c401c9a90a8c5fd8057aecec5d73ab4
Felipe Cavalca <flpsnocvla@gmail.com>
2025-03-26 10:56:13 -0300

    Aprimora validação de formulário de login e adiciona feedback visual ao usuário

--- app/js/pages/index.js
+++ app/js/pages/index.js
@@ -1,18 +1,38 @@
+var originalTextButton;
+
 const bifrost = new Bifrost(
     (bifrost) => {
+        bifrost.replaceTextInElement("body", bifrost.config);
     },
     (bifrost) => {
-        bifrost.replaceTextInElement("body", bifrost.config);
-
         bifrost.form("form", () => {
-            // return false;
-        }, async (response) => {
-            let jsonRes = await response.json();
-            document.querySelector("c-alert").style.display = 'block'
-            document.querySelector("#response-form").innerHTML = jsonRes.message;
+            let invalido = document.querySelector("input:invalid");
 
-            if (jsonRes.isSuccess) {
-                window.location.href = './home.html';
+            if (invalido) {
+                alert().error("Preencha todos os campos corretamente");
+                return false;
+            }
+
+            let button = document.querySelector("button[type=submit]");
+            button.disabled = true;
+            originalTextButton = button.innerHTML;
+            button.innerHTML = "Entrando...";
+            return true;
+        }, async (response) => {
+            let data = await response.json();
+
+            let button = document.querySelector("button[type=submit]");
+            button.disabled = false;
+            button.innerHTML = originalTextButton;
+
+            if (data.isSuccess) {
+                alert().success("Login realizado com sucesso, você será redirecionado para a página inicial");
+                document.querySelector("form").reset();
+                setInterval(() => {
+                    window.location.href = "home.html";
+                }, 2000);
+            } else {
+                alert().error(data.message);
             }
         });
     }
