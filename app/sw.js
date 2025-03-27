bf4a143539d5e4a27763f2cba7c34b0f14ad0038
Felipe Cavalca <flpsnocvla@gmail.com>
2025-03-27 19:05:17 -0300

    Adiciona interceptor de Service Worker para redirecionamento em caso de autorização negada

--- app/sw.js
+++ app/sw.js
@@ -2,6 +2,20 @@ self.addEventListener("install", async event => {
 
 });
 
-self.addEventListener("fetch", async event => {
-
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).then(response => {
+            if (response.status === 401) {
+                // Envia uma mensagem para todas as abas abertas
+                self.clients.matchAll().then(clients => {
+                    clients.forEach(client => {
+                        client.postMessage({ type: 'unauthorized' });
+                    });
+                });
+            }
+            return response;
+        }).catch(error => {
+            return new Response('Erro', { status: 500 });
+        })
+    );
 });
