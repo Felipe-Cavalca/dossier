be24b2d8b5461ecc1301752baff641a4f440e18a
Felipe Cavalca <flpsnocvla@gmail.com>
2025-04-09 23:35:05 -0300

    Renomeia a variável de classe "cachedRole" para "roleClass" e ajusta a lógica de inicialização do papel para melhorar a legibilidade e a consistência do código.

--- api/Class/User.php
+++ api/Class/User.php
@@ -17,7 +17,7 @@ class User
     public Email $email;
     public string $password;
     private UUID $roleId;
-    private ?Role $cachedRole = null;
+    private ?Role $roleClass = null;
 
     public function __construct(
         ?UUID $id = null,
@@ -70,10 +70,10 @@ class User
 
     private function getRole(): Role
     {
-        if ($this->cachedRole === null) {
-            $this->cachedRole = new Role($this->roleId);
+        if (empty($this->roleClass)) {
+            $this->roleClass = new Role($this->roleId);
         }
-        return $this->cachedRole;
+        return $this->roleClass;
     }
 
     public function validatePassword(string $password): bool
